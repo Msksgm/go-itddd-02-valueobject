40e14446eebca0ad48c9f820d62554487e0dd3a5
Msksgm <msksgm3124@gmail.com>
2022-02-22 15:40:59 +0900

    add: the test of different currency

--- domain/model/money/money.go
+++ domain/model/money/money.go
@@ -1,6 +1,11 @@
 package money
 
-import "github.com/shopspring/decimal"
+import (
+	"fmt"
+
+	"github.com/Msksgm/itddd-go-02-valueobject/iterrors"
+	"github.com/shopspring/decimal"
+)
 
 type Money struct {
 	amount   decimal.Decimal
@@ -17,6 +22,11 @@ func NewMoney(amount decimal.Decimal, currency string) (_ *Money, err error) {
 }
 
 func (money *Money) Add(arg Money) (_ *Money, err error) {
+	defer iterrors.Wrap(&err, "money.Add(%v)", arg)
+	if money.currency != arg.currency {
+		err = fmt.Errorf("currency is different between money:%s and arg:%s", money.currency, arg.currency)
+		return nil, err
+	}
 	newMoney, err := NewMoney(money.amount.Add(arg.amount), money.currency)
 	return newMoney, err
 }
--- domain/model/money/money_test.go
+++ domain/model/money/money_test.go
@@ -1,6 +1,7 @@
 package money
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -26,29 +27,50 @@ func TestNewMoney(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	amount1, err := decimal.NewFromString("100")
-	if err != nil {
-		t.Fatal(err)
-	}
-	currency := "JPY"
-	money1 := &Money{amount: amount1, currency: currency}
-	amount2, err := decimal.NewFromString("200")
-	if err != nil {
-		t.Fatal(err)
-	}
-	money2 := &Money{amount: amount2, currency: currency}
-	got, err := money1.Add(*money2)
-	if err != nil {
-		t.Error(err)
-	}
+	t.Run("success", func(t *testing.T) {
+		amount1, err := decimal.NewFromString("100")
+		if err != nil {
+			t.Fatal(err)
+		}
+		currency := "JPY"
+		money1 := &Money{amount: amount1, currency: currency}
+		amount2, err := decimal.NewFromString("200")
+		if err != nil {
+			t.Fatal(err)
+		}
+		money2 := &Money{amount: amount2, currency: currency}
+		got, err := money1.Add(*money2)
+		if err != nil {
+			t.Error(err)
+		}
 
-	wantAmount, err := decimal.NewFromString("300")
-	if err != nil {
-		t.Fatal(err)
-	}
-	want := &Money{amount: wantAmount, currency: currency}
+		wantAmount, err := decimal.NewFromString("300")
+		if err != nil {
+			t.Fatal(err)
+		}
+		want := &Money{amount: wantAmount, currency: currency}
 
-	if diff := cmp.Diff(got, want, cmp.AllowUnexported(Money{})); diff != "" {
-		t.Errorf("mismatch (-want, +got):\n%s", diff)
-	}
+		if diff := cmp.Diff(got, want, cmp.AllowUnexported(Money{})); diff != "" {
+			t.Errorf("mismatch (-want, +got):\n%s", diff)
+		}
+	})
+	t.Run("fail currency is different", func(t *testing.T) {
+		amount1, err := decimal.NewFromString("100")
+		if err != nil {
+			t.Fatal(err)
+		}
+		currency := "JPY"
+		money1 := &Money{amount: amount1, currency: currency}
+		amount2, err := decimal.NewFromString("20.0")
+		if err != nil {
+			t.Fatal(err)
+		}
+		currency2 := "USD"
+		arg := &Money{amount: amount2, currency: currency2}
+		_, err = money1.Add(*arg)
+		want := fmt.Sprintf("money.Add(%v): currency is different between money:%s and arg:%s", *arg, money1.currency, arg.currency)
+		if got := err.Error(); got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	})
 }
